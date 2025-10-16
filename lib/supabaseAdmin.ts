17528b03a12b6f222f0ae2c1ffa2a112536811d3
Tom Birch <57155566+birchy8624@users.noreply.github.com>
2025-10-16 18:09:44 +0200

    fix: make supabase link script non-interactive

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+import type { Database } from '@/types/supabase'
+
 export const supabaseAdmin = () =>
-  createClient(
+  createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE!,
     {
