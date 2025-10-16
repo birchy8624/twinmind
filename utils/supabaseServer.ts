cdca7a1031eee47eaff562651a11edf9d9341bd0
Tom Birch <57155566+birchy8624@users.noreply.github.com>
2025-10-16 18:09:33 +0200

    fix: make supabase link script non-interactive

--- utils/supabaseServer.ts
+++ utils/supabaseServer.ts
@@ -1,10 +1,12 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
+import type { Database } from '@/types/supabase'
+
 export const createServerClient = () => {
   const cookieStore = cookies()
 
-  return createServerComponentClient({
+  return createServerComponentClient<Database>({
     cookies: () => cookieStore
   })
 }
