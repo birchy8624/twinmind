1bf726973e68eeb9f744b01d4ff7ae987b3eb749
Tom Birch <57155566+birchy8624@users.noreply.github.com>
2025-10-17 13:07:16 +0200

    Show company names on clients table

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -152,6 +152,7 @@ export type Database = {
       clients: {
         Row: {
           account_status: 'active' | 'inactive' | 'invited' | 'archived'
+          company: string | null
           created_at: string
           id: string
           name: string
@@ -159,6 +160,7 @@ export type Database = {
         }
         Insert: {
           account_status?: 'active' | 'inactive' | 'invited' | 'archived'
+          company?: string | null
           created_at?: string
           id?: string
           name: string
@@ -166,6 +168,7 @@ export type Database = {
         }
         Update: {
           account_status?: 'active' | 'inactive' | 'invited' | 'archived'
+          company?: string | null
           created_at?: string
           id?: string
           name?: string
